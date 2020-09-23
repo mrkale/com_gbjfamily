4e66f204d6d8c0c2a43a88be5b21a4db9df5f43e
Libor Gabaj <libor.gabaj@gmail.com>
2020-09-23 17:52:22 +0200

    Increased minor version and fixed sql file.

--- admin/sql/updates/mysql/2.0.16.sql
+++ admin/sql/updates/mysql/2.0.16.sql
@@ -1,6 +1,6 @@
 /**
  * @package    Joomla.Component
- * @copyright  (c) 2019 Libor Gabaj
+ * @copyright  (c) 2019-2020 Libor Gabaj
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  * @since      3.8
  */
@@ -9,7 +9,7 @@
  * Update agenda tables
  */
 ALTER TABLE `#__gbjfamily_devices`
-DROP COLUMN `type`,
-DROP COLUMN `eth_ifc`,
-DROP COLUMN  `wifi_ifc`,
-DROP COLUMN  `id_port`;
+DROP COLUMN IF EXISTS `type`,
+DROP COLUMN IF EXISTS `eth_ifc`,
+DROP COLUMN IF EXISTS `wifi_ifc`,
+DROP COLUMN IF EXISTS `id_port`;
