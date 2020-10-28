642767a8118f2140d60f448b2ac3d565e7231dac
Libor Gabaj <libor.gabaj@gmail.com>
2020-10-28 18:15:23 +0100

    Fixed language constant in configuration.

--- admin/gbjfamily.php
+++ admin/gbjfamily.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    Joomla.Component
- * @copyright  (c) 2017-2020 Libor Gabaj
+ * @copyright  (c) 2017 Libor Gabaj
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  * @since      3.8
  */
@@ -9,9 +9,5 @@
 // No direct access
 defined('_JEXEC') or die;
 
-require_once JPATH_COMPONENT_ADMINISTRATOR
- . DIRECTORY_SEPARATOR
- . 'helpers'
- . DIRECTORY_SEPARATOR
- . 'main.php';
+require_once JPATH_COMPONENT_ADMINISTRATOR . '/helpers/main.php';
 Helper::launchMainController();
