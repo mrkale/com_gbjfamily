04dfed08a9345ccbf2eca96fe14d0407c37007d6
Libor Gabaj <libor.gabaj@gmail.com>
2020-11-22 11:45:17 +0100

    Added measurement units to the frontend of agenda Facts.

--- site/views/fact/tmpl/default_item.php
+++ site/views/fact/tmpl/default_item.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    Joomla.Component
- * @copyright  (c) 2019 Libor Gabaj
+ * @copyright  (c) 2019-2020 Libor Gabaj
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  * @since      3.8
  */
@@ -12,17 +12,19 @@ defined('_JEXEC') or die;
 $layoutBasePath = Helper::getLayoutBase();
 $tparams = $this->params;
 $pageclass_sfx = htmlspecialchars($tparams->get('pageclass_sfx'));
-$class = strtolower(Helper::getClassPrefix()). '_dl' . $pageclass_sfx;
+$class = strtolower(Helper::getClassPrefix()) . '_dl' . $pageclass_sfx;
 
 $this->item->periodfmt = JText::sprintf('LIB_GBJ_FORMAT_PERIOD_DATE',
-	Helper::formatPeriodDates($this->item->date_on, $this->item->date_off));
+	Helper::formatPeriodDates($this->item->date_on, $this->item->date_off)
+);
 ?>
 <dl class="<?php echo $class; ?>">
-	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field'=>'alias')); ?>
-	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field'=>'date_on')); ?>
-	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field'=>'date_off')); ?>
-	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field'=>'period')); ?>
-	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field'=>'id_domain')); ?>
-	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field'=>'id_info')); ?>
-	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field'=>'id_project')); ?>
+	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field' => 'alias')); ?>
+	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field' => 'id_unit')); ?>
+	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field' => 'date_on')); ?>
+	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field' => 'date_off')); ?>
+	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field' => 'period')); ?>
+	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field' => 'id_domain')); ?>
+	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field' => 'id_info')); ?>
+	<?php echo JLayoutHelper::render('record.field', $this, $layoutBasePath, array('field' => 'id_project')); ?>
 </dl>
--- site/views/facts/tmpl/default_items.php
+++ site/views/facts/tmpl/default_items.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    Joomla.Component
- * @copyright  (c) 2019 Libor Gabaj
+ * @copyright  (c) 2019-2020 Libor Gabaj
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  * @since      3.8
  */
@@ -26,65 +26,80 @@ $showDesc = $tparams->get('show_itemdescription');
 
 <form action="<?php echo htmlspecialchars(JUri::getInstance()->toString()); ?>" method="post" name="adminForm" id="adminForm">
 
-<?php if ($tparams->get('show_filter_bar')) : ?>
+<?php if ($tparams->get('show_filter_bar'))
+:
+?>
 	<?php echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
 <?php endif; ?>
-<?php if ($this->total > 0) : ?>
-		<input type="hidden" name="filter_order" value="" />
-		<input type="hidden" name="filter_order_Dir" value="" />
-		<input type="hidden" name="limitstart" value="" />
-		<input type="hidden" name="task" value="" />
 
-	<div class="table-responsive">
-	<table class="table table-hover gbjfamily_table<?php echo $pageclass_sfx; ?>" id="recordList">
-		<?php if ($tparams->get('show_filter_stats')) : ?>
-			<caption><?php echo $this->htmlStatistics(); ?></caption>
+<?php if ($this->total > 0)
+:
+?>
+<input type="hidden" name="filter_order" value="" />
+<input type="hidden" name="filter_order_Dir" value="" />
+<input type="hidden" name="limitstart" value="" />
+<input type="hidden" name="task" value="" />
+
+<div class="table-responsive">
+<table class="table table-hover gbjfamily_table<?php echo $pageclass_sfx; ?>" id="recordList">
+
+	<?php if ($tparams->get('show_filter_stats'))
+	:
+	?>
+	<caption><?php echo $this->htmlStatistics(); ?></caption>
+	<?php endif; ?>
+	<thead>
+		<tr>
+			<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'	=> 'sequence')); ?>
+			<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'	=> 'title')); ?>
+			<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'	=> 'alias')); ?>
+			<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'	=> 'date_on, date_off')); ?>
+			<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'	=> 'period')); ?>
+			<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'	=> 'id_domain')); ?>
+			<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'	=> 'id_info')); ?>
+			<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'	=> 'id_project')); ?>
+
+	<?php if ($showDesc)
+	:
+	?>
+		<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields' => 'description')); ?>
+	<?php endif; ?>
+		</tr>
+	</thead>
+	<tfoot>
+		<tr>
+			<td <?php echo $this->htmlAttribute('colspan', $this->columns); ?>>
+				<?php echo $this->pagination->getListFooter(); ?>
+			</td>
+		</tr>
+	</tfoot>
+	<tbody>
+	<?php foreach ($this->items as $this->item->sequence => $this->item)
+	:
+	?>
+		<?php
+			$this->item->sequence += $this->pagination->limitstart;
+			$this->item->periodfmt = JText::sprintf('LIB_GBJ_FORMAT_PERIOD_DATE',
+				Helper::formatPeriodDates($this->item->date_on, $this->item->date_off)
+			);
+		?>
+		<tr <?php echo ($this->item->featured ? $this->htmlAttribute('class', $featuredClass) : ''); ?>>
+			<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields' => 'sequence')); ?>
+			<?php echo JLayoutHelper::render('grid.items_detail', $this, $layoutBasePath, array('fields' => 'title')); ?>
+			<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields' => 'alias')); ?>
+			<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields' => 'date_on, date_off')); ?>
+			<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields' => 'period')); ?>
+			<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields' => 'id_domain')); ?>
+			<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields' => 'id_info')); ?>
+			<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields' => 'id_project')); ?>
+
+		<?php if ($showDesc)
+		:
+		?>
+			<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields' => 'description')); ?>
 		<?php endif; ?>
-		<thead>
-			<tr>
-				<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'=>'sequence')); ?>
-				<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'=>'title')); ?>
-				<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'=>'alias')); ?>
-				<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'=>'date_on, date_off')); ?>
-				<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'=>'period')); ?>
-				<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'=>'id_domain')); ?>
-				<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'=>'id_info')); ?>
-				<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'=>'id_project')); ?>
-			<?php if ($showDesc) : ?>
-				<?php echo JLayoutHelper::render('grid.headers', $this, $layoutBasePath, array('fields'=>'description')); ?>
-			<?php endif; ?>
-
 			</tr>
-		</thead>
-		<tfoot>
-			<tr>
-				<td <?php echo $this->htmlAttribute('colspan', $this->columns); ?>
-				>
-					<?php echo $this->pagination->getListFooter(); ?>
-				</td>
-			</tr>
-		</tfoot>
-		<tbody>
-		<?php foreach ($this->items as $this->item->sequence => $this->item) : ?>
-			<?php
-				$this->item->sequence += $this->pagination->limitstart;
-				$this->item->periodfmt = JText::sprintf('LIB_GBJ_FORMAT_PERIOD_DATE',
-					Helper::formatPeriodDates($this->item->date_on, $this->item->date_off));
-			?>
-			<tr <?php echo ($this->item->featured ? $this->htmlAttribute('class', $featuredClass) : ''); ?>>
-				<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields'=>'sequence')); ?>
-				<?php echo JLayoutHelper::render('grid.items_detail', $this, $layoutBasePath, array('fields'=>'title')); ?>
-				<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields'=>'alias')); ?>
-				<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields'=>'date_on, date_off')); ?>
-				<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields'=>'period')); ?>
-				<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields'=>'id_domain')); ?>
-				<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields'=>'id_info')); ?>
-				<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields'=>'id_project')); ?>
-			<?php if ($showDesc) : ?>
-				<?php echo JLayoutHelper::render('grid.items', $this, $layoutBasePath, array('fields'=>'description')); ?>
-			<?php endif; ?>
-			</tr>
-		<?php endforeach; ?>
+	<?php endforeach; ?>
 		</tbody>
 	</table>
 	</div>
